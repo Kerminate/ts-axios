5bb60ba4440a2dad4a75851ef9620f42f017e8be
Kerminate <kepeilin@outlook.com>
2019-06-06 09:59:49 +0800

    feat: 增加 xhr 的一些基本配置

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { request } from "https";
+
 export type Method = 'get' | 'GET'
 | 'delete' | 'Delete'
 | 'head' | 'HEAD'
@@ -11,4 +13,19 @@ export interface AxiosRequestConfig {
   method?: Method
   data?: any
   params?: any
+  headers?: any
+  responseType?: XMLHttpRequestResponseType
+}
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {
+
 }
