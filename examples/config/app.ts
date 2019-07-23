fcb7002567f009ddbee0cde12a50d13e65d5be76
Kerminate <kepeilin@outlook.com>
2019-07-23 22:09:49 +0800

    feat: 请求和响应配置化

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,3 +1,4 @@
+import { AxiosTransformer } from './../../src/types/index';
 import axios from '../../src/index';
 import qs from 'qs';
 
@@ -11,6 +12,31 @@ axios({
   }),
   headers: {
     test: '321'
+  }
+}).then(res => {
+  console.log(res.data);
+});
+
+axios({
+  transformRequest: [
+    function(data) {
+      return qs.stringify(data);
+    },
+    ...(axios.defaults.transformRequest as AxiosTransformer[])
+  ],
+  transformResponse: [
+    ...(axios.defaults.transformResponse as AxiosTransformer[]),
+    function(data) {
+      if (typeof data === 'object') {
+        data.b = 2;
+      }
+      return data;
+    }
+  ],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
   }
 }).then(res => {
   console.log(res.data);
